656f3a7947e86ae2b0d54207c90c87808fbd6903
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-11-07 20:58:02 +0700

    Update 4
    - Penambahan Bootstrap 4.5.0
    - Perbaikan Navbar

--- index.php
+++ index.php
@@ -5,12 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda | Lapor Sampah</title>
-    <link rel="stylesheet" href="assets/style.css">
+
+    <!-- Muat CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom-style.css">
 </head>
 
 <body>
     <?php include "navbar.php" ?>
-    <h1>Selamat Datang di Website Lapor Sampah</h1>
+
+    <div class="card ml-md-5 mr-md-5">
+        <div class="card-body text-sm-center shadow-lg">
+            <h1>Selamat Datang di Website Lapor Sampah</h1>
+        </div>
+
+    </div>
+
 </body>
+<!-- Muat JS -->
+<script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -1,10 +1,20 @@
-<nav>
-    <ul>
-        <li>Lapor Sampah</li>
-        <ul>
-            <li>Daftar</li>
-            <li>Masuk</li>
-            <li>Tentang Kami</li>
-        </ul>
+<nav class="navbar sticky-top navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">
+        <img src="assets/img/bin.svg" width="30" height="30" class="d-inline-block align-top" alt="Lapor Sampah">
+        Lapor Sampah
+    </a>
+    <ul class="nav nav-pills nav-fill">
+        <li class="nav-item">
+            <!-- <a class="nav-link active" href="#">Daftar</a> -->
+            <button class="btn btn-outline-light mr-md-2" type="button">Daftar</button>
+        </li>
+        <li class="nav-item">
+            <!-- <a class="nav-link active" href="#">Masuk</a> -->
+            <button class="btn btn-outline-light mr-md-2 ml-md-2" type="button">Masuk</button>
+        </li>
+        <li class="nav-item">
+            <!-- <a class="nav-link active" href="#">Tentang Kami</a> -->
+            <button class="btn btn-outline-light ml-md-2" type="button">Tentang Kami</button>
+        </li>
     </ul>
 </nav>
