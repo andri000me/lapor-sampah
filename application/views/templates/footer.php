77dbb42fd1b4fc1d0f3ea817eb905591a081aa10
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-11-10 12:32:44 +0700

    Update footer.php

---
+++ application/views/templates/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color darken-3 pt-4">
+
+    <!-- Footer Elements -->
+    <div class="container">
+
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+            <h4>Lapor Sampah</h4>
+        </div>
+        <!--Grid row-->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">&copy;2020
+        <a href="#">Lapor Sampah</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
