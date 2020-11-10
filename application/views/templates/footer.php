a6d793bb545dbd121f6a198c83c51c1da0dec324
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-11-10 12:38:55 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,23 +1,55 @@
 <!-- Footer -->
-<footer class="page-footer font-small mdb-color darken-3 pt-4">
+<footer class="page-footer font-small cyan darken-3">
 
-    <!-- Footer Elements -->
-    <div class="container">
+  <!-- Footer Elements -->
+  <div class="container">
 
-        <!--Grid row-->
-        <div class="row d-flex justify-content-center">
-            <h4>Lapor Sampah</h4>
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fa fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fa fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fa fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Pinterest-->
+          <a class="pin-ic">
+            <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
         </div>
-        <!--Grid row-->
+      </div>
+      <!-- Grid column -->
 
     </div>
-    <!-- Footer Elements -->
+    <!-- Grid row-->
 
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">&copy;2020
-        <a href="#">Lapor Sampah</a>
-    </div>
-    <!-- Copyright -->
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">&copy; 2020 Copyright:
+    <a href="#"> Lapor Sampah</a>
+  </div>
+  <!-- Copyright -->
 
 </footer>
 <!-- Footer -->
