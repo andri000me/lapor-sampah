bbcb262a094200084789add265c270c4785880ab
danisfaiall <>
2020-11-10 13:42:46 +0700

    Update index.php
    Membuat jumbotron

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -13,8 +13,15 @@
 
 <body>
 
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">SELAMAT DATANG</h1>
+            <p class="lead">Di Web Lapor Sampah</p>
+        </div>
+    </div>
 
-    <div class="container mt-md-5">
+
+    <!-- <div class="container mt-md-5">
         <div class="row">
             <div class="col">
                 <div class="card text-center">
@@ -24,7 +31,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 </body>
