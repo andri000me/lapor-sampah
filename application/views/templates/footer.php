43cc27d1be0efebcc03889691d2fa20f371c5783
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-12-24 11:56:29 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,24 +3,46 @@
 <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 
-<section class="bg-dark text-white pt-1 pb-4">
+<section class="bg-dark text-white pt-1 pb-4 mt-4">
     <div class="container mt-md-5">
-        <div class="row text-center">
-            <div class="col-sm">
-                <label>Hubungi Kami</label>
+        <div class="row">
+            <div class="col-sm text-left">
+                <h4>Hubungi Kami</h4>
                 <p>Jl. Ireda</p>
                 <p>No 821 Keparakan</p>
                 <p>Mergangsan, Kota Yogyakarta</p>
                 <p>DI Yogyakarta</p>
             </div>
-
-            <div class="col-sm">
-                <a href="facebook.com" class="flex-column">
-                    <div class="justify-content-start align-content-center">
-                        <span class="fab fa-facebook fa-fw fa-2x"></span>
-                        <span>Sosial Media</span>
+            <div class="col-sm text-left">
+                <div class="container">
+                    <h4>Media Sosial</h4>
+                    <div class="w-100">
+                        <div class="col">
+                            <a href="https://www.facebook.com">
+                                <button class="btn text-white text-center">
+                                    <i class="fab fa-facebook fa-fw"></i>
+                                    Facebook
+                                </button>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="https://www.twitter.com">
+                                <button class="btn text-white">
+                                    <i class="fab fa-twitter fa-fw"></i>
+                                    Twitter
+                                </button>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="https://www.instagram.com">
+                                <button class="btn text-white">
+                                    <i class="fab fa-instagram fa-fw"></i>
+                                    Instagram
+                                </button>
+                            </a>
+                        </div>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
     </div>
@@ -28,6 +50,6 @@
 
 <section class="bg-light pt-2 pb-1 text-center">
     <div class="col-sm">
-        <p>Copyright &copy; Lapor Sampah Allright Reserved</p>
+        <p>&copy; 2020 Lapor Sampah Allright Reserved</p>
     </div>
 </section>
